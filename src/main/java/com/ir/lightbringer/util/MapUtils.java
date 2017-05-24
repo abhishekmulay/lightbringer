0b64d25a51148364d999e47d572c0035dcfa6ca2
Abhishek Mulay <mulay.a@husky.neu.edu>
2017-05-24 14:05:36 -0400

    TFIDF done

---
+++ src/main/java/com/ir/lightbringer/util/MapUtils.java
@@ -0,0 +1,7 @@
+package com.ir.lightbringer.util;
+
+/**
+ * Created by Abhishek Mulay on 5/24/17.
+ */
+public class MapUtils {
+}
---
+++ src/main/java/com/ir/lightbringer/vectorspacemodels/TfIdfCalculator.java
@@ -0,0 +1,7 @@
+package com.ir.lightbringer.vectorspacemodels;
+
+/**
+ * Created by Abhishek Mulay on 5/24/17.
+ */
+public class TfIdfCalculator {
+}
